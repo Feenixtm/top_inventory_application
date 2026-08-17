952c12d2cfb2b98e08262c29459540e291edc0f5
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-17 16:20:44 -0700

    Installed Tailwind and added ability to delete items from inventory

---
+++ models/mockDatav2.js
@@ -0,0 +1,19 @@
+const mockDatav2 = [
+    {
+        slotID: 4,
+        itemName: "Dirt",
+        amount: 64,
+    },
+    {
+        slotID: 7,
+        itemName: "Stone",
+        amount: 16,
+    },
+    {
+        slotID: 12,
+        itemName: "Sand",
+        amount: 32,
+    },
+]
+
+export default mockDatav2
--- routes/routes.js
+++ routes/routes.js
@@ -2,29 +2,58 @@ import express from "express";
 const router = express.Router();
 
 import mockData from "../models/mockData.js";
+import mockDatav2 from "../models/mockDatav2.js";
 
 router.get("/", (req, res) => {
-    res.render("inventory", { data: mockData });
+    res.render("inventory", { data: mockDatav2 });
 })
 
 router.get("/edit-slot/:id", (req, res) => {
-    const slotID = req.params.id;
+    const id = Number(req.params.id);
     
-    res.render("edit-slot", { slotID: slotID });
+    res.render("edit-slot", { slotID: id });
 })
 // ----------------------------------------------
 
 router.post("/edit-slot/:id", (req, res) => {
-    const slotID = req.params.id;
+    const id = Number(req.params.id);
 
     const blockType = req.body.blockType;
     const amount = req.body.amount;
 
-    mockData[(slotID - 1)] = {
-        slotID: slotID,
-        itemName: blockType,
-        amount: amount,
+    // IF ITEM DOESNT EXIST ON THIS INDEX THEN PUSH IT TO THE ARRAY. OTHERWISE, EDIT THE PRE-EXISTING ONE
+    // WIP (BELOW)
+
+    const index = mockDatav2.findIndex(slot => slot.id === Number(id));
+    console.log(index);
+
+    if (index === -1) {
+        const newItem = {
+            slotID: id,
+            itemName: blockType,
+            amount: amount,
+        }
+
+        console.log(newItem);
+
+        mockDatav2.push(newItem)
+    } else {
+        mockDatav2[index] = {
+            slotID: id,
+            itemName: blockType,
+            amount: amount,
+        }
     }
+
+    res.redirect("/");
+})
+
+router.post("/delete-slot-item/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = mockDatav2.findIndex(slot => slot.slotID === id)
+
+    mockDatav2.splice(index, 1);
 
     res.redirect("/");
 })
