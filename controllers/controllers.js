f9e5ecb1d642569f6ee670cbfcecfd8fd313c67c
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-18 13:36:35 -0700

    Moved existing logic from routes.js to controllers.js and imported them over

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,63 @@
-export const testMiddleware = (req, res) => {
-    console.log("Testing middleware...")
+import mockDatav2 from "../models/mockDatav2.js";
+
+export const getInventory = (req, res) => {
+    res.render("inventory", { data: mockDatav2 })
 }
+
+// -----------------------------------------------
+
+export const getSlot = (req, res) => {
+    const id = Number(req.params.id);
+    
+    res.render("edit-slot", { slotID: id });
+}
+
+// -----------------------------------------------
+
+export const updateSlot = (req, res) => {
+    const id = Number(req.params.id);
+
+    const blockType = req.body.blockType;
+    const amount = req.body.amount;
+
+    // IF ITEM DOESNT EXIST ON THIS INDEX THEN PUSH IT TO THE ARRAY. OTHERWISE, EDIT THE PRE-EXISTING ONE
+    // WIP (BELOW)
+
+    const index = mockDatav2.findIndex(slot => slot.slotID === Number(id));
+    console.log(index);
+
+    if (index === -1) {
+        const newItem = {
+            slotID: id,
+            itemName: blockType,
+            amount: amount,
+        }
+
+        console.log(newItem);
+
+        mockDatav2.push(newItem)
+    } else {
+        mockDatav2[index] = {
+            slotID: id,
+            itemName: blockType,
+            amount: amount,
+        }
+    }
+
+    res.redirect("/");
+}
+
+// -----------------------------------------------
+ 
+export const deleteSlot = (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = mockDatav2.findIndex(slot => slot.slotID === id)
+
+    mockDatav2.splice(index, 1);
+
+    res.redirect("/");
+}
+
+// -----------------------------------------------
 
--- routes/routes.js
+++ routes/routes.js
@@ -6,61 +6,13 @@ import mockDatav2 from "../models/mockDatav2.js";
 
 import * as controller from "../controllers/controllers.js";
 
-router.get("/controller-test", controller.testMiddleware);
+router.get("/", controller.getInventory);
 
-router.get("/", (req, res) => {
-    res.render("inventory", { data: mockDatav2 });
-})
+router.get("/edit-slot/:id", controller.getSlot);
 
-router.get("/edit-slot/:id", (req, res) => {
-    const id = Number(req.params.id);
-    
-    res.render("edit-slot", { slotID: id });
-})
-// ----------------------------------------------
+router.post("/edit-slot/:id", controller.updateSlot);
 
-router.post("/edit-slot/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const blockType = req.body.blockType;
-    const amount = req.body.amount;
-
-    // IF ITEM DOESNT EXIST ON THIS INDEX THEN PUSH IT TO THE ARRAY. OTHERWISE, EDIT THE PRE-EXISTING ONE
-    // WIP (BELOW)
-
-    const index = mockDatav2.findIndex(slot => slot.id === Number(id));
-    console.log(index);
-
-    if (index === -1) {
-        const newItem = {
-            slotID: id,
-            itemName: blockType,
-            amount: amount,
-        }
-
-        console.log(newItem);
-
-        mockDatav2.push(newItem)
-    } else {
-        mockDatav2[index] = {
-            slotID: id,
-            itemName: blockType,
-            amount: amount,
-        }
-    }
-
-    res.redirect("/");
-})
-
-router.post("/delete-slot-item/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const index = mockDatav2.findIndex(slot => slot.slotID === id)
-
-    mockDatav2.splice(index, 1);
-
-    res.redirect("/");
-})
+router.post("/delete-slot-item/:id", controller.deleteSlot);
 
 export default router;
 
